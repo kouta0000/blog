7fd6e451165dfccd2b429fd50580dcd2bc71b6b0
kota <kukkmathphys1236@gmail.com>
2025-09-06 18:26:28 +0900

    a

--- src/components/common/Advertisement.svelte
+++ src/components/common/Advertisement.svelte
@@ -1,27 +1,35 @@
 <script lang='ts'>
+    import { onMount } from "svelte";
     interface Props {
         size?:string;
     }
+    const srcs = {
+        s:'https://adm.shinobi.jp/s/e1e5398aa8ca972b65698449d0da0cfb',
+        m:'https://adm.shinobi.jp/s/95f151d5688017250c7c95003abbbbd9',
+        l:'https://adm.shinobi.jp/s/a1cebbffb885b81982438fe98b956a05',
+    }
     let { size='s' }:Props = $props();
+    onMount(()=>{
+        const script = document.createElement('script');
+        switch(size) {
+            case 's':
+                script.src = srcs.s;
+                break;
+            case 'm':
+                script.src = srcs.m;
+                break;
+            case 'l':
+                script.src = srcs.l;
+                break;
+        }
+        script.onload = ()=>{
+            if(container) {
+                container.appendChild(script);
+            }
+        }
+    })
+    let container:HTMLDivElement | undefined = $state();
 </script>
-{#if size ==='s'} 
-<div class="w-full flex justify-center">
-    <div class="admax-ads" data-admax-id="e1e5398aa8ca972b65698449d0da0cfb" style="display:inline-block;"></div>
-<script type="text/javascript">(admaxads = window.admaxads || []).push({admax_id: "e1e5398aa8ca972b65698449d0da0cfb",type: "banner"});</script>
-<script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>
+<div bind:this={container} class='w-full flex justify-center'>
+
 </div>
-{/if}
-{#if size === 'm'}
-<div class="w-full flex justify-center">
-<div class="admax-ads" data-admax-id="95f151d5688017250c7c95003abbbbd9" style="display:inline-block;"></div>
-<script type="text/javascript">(admaxads = window.admaxads || []).push({admax_id: "95f151d5688017250c7c95003abbbbd9",type: "banner"});</script>
-<script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>
-</div>
-{/if}
-{#if size==='l'}
-<div class="w-full flex justify-center">
-<div class="admax-ads" data-admax-id="a1cebbffb885b81982438fe98b956a05" style="display:inline-block;"></div>
-<script type="text/javascript">(admaxads = window.admaxads || []).push({admax_id: "a1cebbffb885b81982438fe98b956a05",type: "banner"});</script>
-<script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>
-</div>
-{/if}
