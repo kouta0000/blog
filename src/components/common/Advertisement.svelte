81ec14625e84907abb6bda2362a8fdae999b070b
kota <kukkmathphys1236@gmail.com>
2025-09-06 18:29:37 +0900

    a

--- src/components/common/Advertisement.svelte
+++ src/components/common/Advertisement.svelte
@@ -1,35 +1,26 @@
 <script lang='ts'>
     import { onMount } from "svelte";
-    interface Props {
-        size?:string;
-    }
-    const srcs = {
-        s:'https://adm.shinobi.jp/s/e1e5398aa8ca972b65698449d0da0cfb',
-        m:'https://adm.shinobi.jp/s/95f151d5688017250c7c95003abbbbd9',
-        l:'https://adm.shinobi.jp/s/a1cebbffb885b81982438fe98b956a05',
-    }
-    let { size='s' }:Props = $props();
-    onMount(()=>{
-        const script = document.createElement('script');
-        switch(size) {
-            case 's':
-                script.src = srcs.s;
-                break;
-            case 'm':
-                script.src = srcs.m;
-                break;
-            case 'l':
-                script.src = srcs.l;
-                break;
-        }
-        script.onload = ()=>{
-            if(container) {
-                container.appendChild(script);
-            }
-        }
-    })
-    let container:HTMLDivElement | undefined = $state();
-</script>
-<div bind:this={container} class='w-full flex justify-center'>
 
-</div>
+    interface Props {
+        size?: string;
+    }
+
+    const srcs = {
+        s: 'https://adm.shinobi.jp/s/e1e5398aa8ca972b65698449d0da0cfb',
+        m: 'https://adm.shinobi.jp/s/95f151d5688017250c7c95003abbbbd9',
+        l: 'https://adm.shinobi.jp/s/a1cebbffb885b81982438fe98b956a05',
+    };
+
+    let { size = 's' }: Props = $props();
+    let container: HTMLDivElement | undefined = $state();
+
+    onMount(() => {
+        if (container) {
+            const script = document.createElement('script');
+            script.src = srcs[size as keyof typeof srcs] || srcs.s;
+            container.appendChild(script);
+        }
+    });
+</script>
+
+<div bind:this={container} class='w-full flex justify-center'></div>
