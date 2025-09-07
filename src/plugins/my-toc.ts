f223785acae153f6a0bd002541c1102522ee16e7
kota <kukkmathphys1236@gmail.com>
2025-09-07 18:02:27 +0900

    a

--- src/plugins/my-toc.ts
+++ src/plugins/my-toc.ts
@@ -1,24 +1,22 @@
 import { visit } from 'unist-util-visit';
+
+
 import type { Root } from 'mdast';
 import type { Heading } from 'mdast';
 import type { LeafDirective } from 'mdast-util-directive';
 import type { Transformer } from 'unified';
-
 export default function MyToc():Transformer<Root> {
   return (tree:Root, file) => {
     visit(tree, 'leafDirective', (node) => {
+        console.log('aaaa')
       // ::toc ディレクティブの場合、TOC.astro コンポーネントに変換
       if (node.type === 'leafDirective' && node.name === 'toc') {
-        node.data = {
-            hName:'div',
-            hProperties: {
-                class: ['w-100', 'h-20', 'bg-pink-400']
-            },
-            hChildren: [
-                { type: 'text', value: 'TOC placeholder' }
-              ]
-        }
-    }
+        const tocNode = node as LeafDirective;
+        const data = tocNode.data || (tocNode.data = {});
+        
+        // MarkdownノードをAstroコンポーネントに変換するメタデータを追
+        data.hName = 'TOC'
+        data.hProperties = {id: file.basename?.split('.')[0] }
   });
 }
 }
