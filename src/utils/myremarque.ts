757c5d1db4e62837722e5f6b704eec282d190a50
kota <kukkmathphys1236@gmail.com>
2025-09-18 01:44:25 +0900

    a

--- src/utils/myremarque.ts
+++ src/utils/myremarque.ts
@@ -7,19 +7,19 @@ export default function myremarque(): Transformer<Root> {
   return (tree: Root) => {
     visit(tree, 'containerDirective', (node) => {
       if (node.name !== 'remarque') return;
-
+      const children = node.data?.hChildren ?? [];
       const title = node.attributes?.title || 'Remarque';
       const titleNode = h('h4', { class: 'text-sm' }, title);
       const chatbubbleNode = h('div', {
         class: 'chat-bubble bg-teal-50 p-4 text-sm',
-      }, [titleNode]);
-      const children = node.data?.hChildren ?? [];
+      }, [titleNode,...children]);
+      
       node.data = {
         hName: 'div',
         hProperties: {
           class: 'chat chat-end w-full p-10',
         },
-        hChildren: [chatbubbleNode, ...children],
+        hChildren: [chatbubbleNode],
       };
     });
   };
