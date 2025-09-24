3c8bb65b2d24fcc98853f5656c8de4e9f8eab334
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-09-24 11:03:21 +0900

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -121,7 +121,7 @@ export default defineConfig({
   // https://docs.astro.build/en/guides/prefetch/
   prefetch: true,
   // ! Please remember to replace the following site property with your own domain
-  site: "http://astrocitrus.artemkutsan.pp.ua/",
+  site: 'https://kotablog.netlify.app',
   vite: {
     build: {
       sourcemap: true, // Source maps generation
