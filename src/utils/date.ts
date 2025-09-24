4db541d10fa53dda64454f61bdfeb9a1e0d6c8f6
kota <kukkmathphys1236@gmail.com>
2025-09-25 01:20:18 +0900

    a

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -8,7 +8,13 @@ export function getFormattedDate(
 	if (date === undefined) {
 		return "Invalid Date";
 	}
+	const today = new Date();
+	const jstDate = new Date(today.getTime() + 9 * 60 * 60 * 1000);
 
+	if(date.getFullYear() === jstDate.getFullYear() && jstDate.getMonth() === jstDate.getMonth() && (jstDate.getDate() - date.getDate()) <= 7) {
+		const days = jstDate.getDate() - date.getDate();
+		return days === 0 ? 'きょう': days.toString() + '日前';
+	}
 	return new Intl.DateTimeFormat(siteConfig.date.locale, {
 		...(siteConfig.date.options as Intl.DateTimeFormatOptions),
 		...options,
@@ -19,5 +25,5 @@ export function collectionDateSort(
 	a: CollectionEntry<"post" | "note">,
 	b: CollectionEntry<"post" | "note">,
 ) {
-	return b.data.publishDate.getTime() - a.data.publishDate.getTime();
+	return (b.data.publishDate.getTime()) - a.data.publishDate.getTime();
 }
